6409e858a9ee246ffcaaa2e33ba64bee9458d902
tyleramerson <tlamerson983@gmail.com>
2024-11-25 13:40:50 -0500

    new file:   index.css 	new file:   index.html 	new file:   index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// calc programming
+
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+
+}
+function calculate(){
+
+}
